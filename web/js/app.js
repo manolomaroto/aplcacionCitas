62383f1323b1a9b120a2fc11a377db8f251a07c0
manuel gonzalez <manuelgmaroto@gmail.com>
2016-10-15 21:35:36 +0200

    Aplicado angular y funcionando

--- web/js/app.js
+++ web/js/app.js
@@ -25,7 +25,7 @@ myApp.factory("kartService",function(){
 			kart.push(book);
 		},
 		buy: function(book){
-			alert("Thanks for buying", book.name);
+			alert("Thanks for buying", book.date);
 		}
 	}
 })
@@ -33,50 +33,19 @@ myApp.factory("kartService",function(){
 myApp.factory("bookService",function(){
 	var books = [
 		{
-			imgurl:"trece.jpg",
-			name: "13 rue del Percebe",
-			price: "28.50",
-			binding: "Tapa dura",
-			publisher: "Ediciones B",
-			releaseDate: "2016",
-			details: "Por primera vez se reúnen en un solo volumen todas las páginas que realizó Francisco Ibáñez de su mítica serie 13, Rue del Percebe"
+			date: "Lunes, 2 de Enero a las 13:00",
 		},
 		{
-			imgurl:"tres.jpg",
-			name: "Tres abuelas y un cocinero muerto",
-			price: "18.90",
-			binding: "Tapa blanda",
-			publisher: "Suma",
-			releaseDate: "2015",
-			details: "Siiri, Irma y Anna-Liisa son tres viudas de noventa años residentes en El Bosque del Crepúsculo, un centro privado de apartamentos para la tercera edad de Helsinki."
+			date: "Lunes, 3 de Febrero a las 14:00",
 		},
 		{
-			imgurl:"cuerpos.jpg",
-			name: "Los cuerpos extraños",
-			price: "8.95",
-			binding: "Tapa blanda",
-			publisher: "Destino",
-			releaseDate: "2015",
-			details: "Mientras pasa el fin de semana en familia, el brigada Bevilacqua recibe el aviso de que el cadáver de la alcaldesa de una localidad levantina, cuya desaparición había sido previamente denunciada por el marido, ha sido hallado por unos turistas en la playa"
+			date: "Lunes, 4 de Marzo a las 15:00",
 		},
 		{
-			imgurl:"cumbres.jpg",
-			name: "Cumbres borrascosas",
-			price: "9.95",
-			binding: "Tapa blanda",
-			publisher: "Penguin Cl&aacute;sicos",
-			releaseDate: "2015",
-			details: "Cumbres borrascosas, la épica historia de Catherine y Heathcliff, situada en los sombríos y desolados páramos de Yorkshire, constituye una asombrosa visión metafísica del destino, la obsesión, la pasión y la venganza."
+			date: "Lunes, 5 de Abril a las 16:00",
 		},
-		{
-			imgurl:"lector.jpg",
-			name: "El lector de Julio Verne",
-			price: "19.00",
-			binding: "Tapa blanda",
-			publisher: "Tusquets Editores",
-			releaseDate: "2014",
-			details: "Nino, hijo de guardia civil, tiene nueve años, vive en la casa cuartel de un pueblo de la Sierra Sur de Jaén, y nunca podrá olvidar el verano de 1947."
-		},
+
+		
 	];
 
 	return {
@@ -99,7 +68,7 @@ myApp.controller("kartListCtrl", function($scope, kartService){
 myApp.controller("headerCtrl", function($scope,$location) {
 	$scope.appDetails = {};
 	$scope.appDetails.title = "BooKart";
-	$scope.appDetails.tagline = "Miles de libros a tu disposicion";
+	$scope.appDetails.tagline = "Amplio horario a tu disposición";
 
 	$scope.nav={};
 	$scope.nav.isActive = function(path){
