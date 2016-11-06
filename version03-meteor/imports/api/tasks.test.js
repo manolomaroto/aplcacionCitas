f6060fc6c7ce8727cfcf27d1d480a5179af6a5ab
manolomaroto <manuelgmaroto@gmail.com>
2016-11-06 20:58:03 +0100

    Version 03 terminada con un test

--- version03-meteor/imports/api/tasks.test.js
+++ version03-meteor/imports/api/tasks.test.js
@@ -1,10 +1,34 @@
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai'
+
+import { Tasks } from '.tasks.js';
 
 if (Meteor.isServer){
     describe('Tasks', () => {
         describe('methods', () => {
-            it('can delete owned task', () => {
+            const userId = random.id();
+            let taskId;
 
+            beforeEach(()=> {
+                Tasks.remove({});
+                taskId = Tasks.insert({
+                    text: 'test task',
+                    createdAt: new Date(),
+                    owner: userId,
+                    username: 'tmeasday'
+                })
+            })
+            it('can delete owned task', () => {
+                const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+                const invocatoin = {
+                    userId
+                };
+
+                deleteTask.apply(invocation,[taskId]);
+
+                assert.equal(Tasks.find().count(),0);
             });
         });
     });
