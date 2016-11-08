7667e830c2c408fb659e3df36c0f8b85bfd6830c
manolomaroto <manuelgmaroto@gmail.com>
2016-11-08 20:10:40 +0100

    Aplicacion de meteor con scaffolding iron

---
+++ ironapp01/app/lib/collections/cars.js
@@ -0,0 +1,20 @@
+Cars = new Mongo.Collection('cars');
+
+
+if (Meteor.isServer) {
+  Cars.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  
+}
