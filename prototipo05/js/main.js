b76b6b5aa19e1ac9e05e312a999677b19f3ae374
Manuel Gonzalez <manuelgmaroto@gmail.com>
2018-03-18 19:01:57 +0100

    Realizado merge rama indeMovil

--- prototipo05/js/main.js
+++ prototipo05/js/main.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $(".dropdown-button").dropdown();
     $('.collapsible').collapsible();
+    var options = [
+        {selector: '#adultos', offset: 0,callback: function(el) { Materialize.fadeInImage($(el)); } },
+        {selector: '#infantil', offset: 0,callback: function(el) { Materialize.fadeInImage($(el)); } },
+        {selector: '#parejas', offset: 0,callback: function(el) { Materialize.fadeInImage($(el)); } },
+        {selector: '.page-footer', offset: 0,callback: function(el) { Materialize.fadeInImage($(el)); } }
+        
+      ];
+      Materialize.scrollFire(options);
 });
